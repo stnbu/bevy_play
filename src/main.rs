5da2ce4c53b6d81610539d8730c512f690e1d1b0
Mike Burr <mb@unintuitive.org>
2022-06-21 12:15:24 -0600

    compiles/needz werk/dead "newtonian" (potatonian) functions

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ fn main() {
         .run();
 }
 
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
 #[derive(Component)]
 struct Moon;
 
@@ -41,4 +44,26 @@ fn update_position(mut query: Query<&mut Transform, With<Moon>>, time: Res<Time>
         transform.translation.x = (time.seconds_since_startup() as f32).sin() * 150.;
         transform.translation.y = (time.seconds_since_startup() as f32).cos() * 150.;
     }
+}
+
+fn update_velocity(mut moon_query: Query<(&mut Velocity, &Transform), With<Moon>>) {
+    let (mut velocity, transform) = moon_query.single_mut();
+    //force_vector = transform.translation.x, transform.translation.y;
+    let force_magnitude = transform.translation.x.powf(2.) + transform.translation.y.powf(2.);
+}
+
+fn get_distance(position: &Vec<f32>) -> f32 {
+    ((position[0] as f32).powf(2.) + (position[1] as f32).powf(2.)).powf(0.5)
+}
+
+fn get_force(position: &Vec<f32>) -> f32 {
+    let distance = get_distance(position);
+    1. / distance.powf(2.)
+}
+
+fn get_displacement(position: &Vec<f32>) -> Vec<f32> {
+    let force = get_force(&position);
+    let x_component = position[0] / (position[0] + position[1]);
+    let y_component = position[1] / (position[0] + position[1]);
+    vec![x_component * force, y_component * force]
 }
