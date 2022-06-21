c3a7987f5007272d47181d7aa54146d18487f155
Mike Burr <mb@unintuitive.org>
2022-06-21 10:24:22 -0600

    simpler

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,15 @@
-//! A simplified implementation of the classic game "Breakout"
-//! Gradually morphed into something else (mburr)
+//! Anything-Goes Bevy Playground
 
-use bevy::{
-    core::FixedTimestep,
-    math::{const_vec2, const_vec3},
-    prelude::*,
-};
+use bevy::{math::const_vec3, prelude::*};
 
-const TIME_STEP: f32 = 0.25 / 60.0;
-
-// These constants are defined in `Transform` units.
-// Using the default 2D camera they correspond 1:1 with screen pixels.
-// The `const_vec3!` macros are needed as functions that operate on floats cannot be constant in Rust.
-
-// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
 const BALL_STARTING_POSITION: Vec3 = const_vec3!([0.0, 150.0, 1.0]);
 const BALL_SIZE: Vec3 = const_vec3!([30.0, 30.0, 0.0]);
-const BALL_SPEED: f32 = 400.0;
-const INITIAL_BALL_DIRECTION: Vec2 = const_vec2!([0.5, -0.5]);
-
-const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
-const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup)
-        .add_system_set(
-            SystemSet::new()
-                .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                .with_system(apply_velocity),
-        )
+        .add_system_set(SystemSet::new().with_system(update_position))
         .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
@@ -39,37 +17,26 @@ fn main() {
 #[derive(Component)]
 struct Ball;
 
-#[derive(Component, Deref, DerefMut)]
-struct Velocity(Vec2);
-
 // Add the game's entities to our world
 fn setup(mut commands: Commands) {
     // Cameras
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     // Ball
-    commands
-        .spawn()
-        .insert(Ball)
-        .insert_bundle(SpriteBundle {
-            transform: Transform {
-                scale: BALL_SIZE,
-                translation: BALL_STARTING_POSITION,
-                ..default()
-            },
-            sprite: Sprite {
-                color: BALL_COLOR,
-                ..default()
-            },
+    commands.spawn().insert(Ball).insert_bundle(SpriteBundle {
+        transform: Transform {
+            scale: BALL_SIZE,
+            translation: BALL_STARTING_POSITION,
             ..default()
-        })
-        .insert(Velocity(INITIAL_BALL_DIRECTION.normalize() * BALL_SPEED));
+        },
+        sprite: Sprite { ..default() },
+        ..default()
+    });
 }
 
 // NOTES:
 //  * Without `With<Ball>` -- no complaints, no errors, no odd output, no warnings, just: ball does not move.
-//  * Multiplying by `TIME_STEPS` makes it toooo slooow.
-fn apply_velocity(mut query: Query<&mut Transform, With<Ball>>, time: Res<Time>) {
+fn update_position(mut query: Query<&mut Transform, With<Ball>>, time: Res<Time>) {
     for mut transform in query.iter_mut() {
         transform.translation.x = (time.seconds_since_startup() as f32).sin() * 150.;
         transform.translation.y = (time.seconds_since_startup() as f32).cos() * 150.;
